29585747ae9da27a67548922d1a495c53f85788a
almond Heil <almond@almendra.dev>
2024-07-16 20:34:20 -0600

    lvim: Use night-owl theme

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -5,9 +5,18 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
 lvim.plugins = {
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  {
+    "oxfist/night-owl.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      require("night-owl").setup()
+      vim.cmd.colorscheme("night-owl")
+    end,
+  }
 }
 
 lvim.builtin.lualine.style = "default"
 
-lvim.colorscheme = "moonfly"
+lvim.colorscheme = "night-owl"
