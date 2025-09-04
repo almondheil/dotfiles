c4c5e00c7b8eaafdcb6f5ae50aacee2f6b7022d8
almond Heil <almond@almendra.dev>
2025-09-03 22:03:34 -0600

    nvim: Tweak vimwiki settings for easier editing

--- dot_config/nvim/lua/almond/vimwiki.lua
+++ dot_config/nvim/lua/almond/vimwiki.lua
@@ -1,10 +1,13 @@
--- example wiki definition. 
--- see :h vimwiki_list for more
---
--- local mywiki = {
---   path = '/path/to/wiki',
--- }
+local vim_global = vim.g
 
+-- disable URL hiding
+vim_global.vimwiki_url_maxsave = 0
+
+-- when opening `dir/`, open/create `dir/main` instead
+vim_global.vimwiki_dir_link = 'main'
+
+-- one wiki definition. 
+-- see :h vimwiki_list for more
 local notes = {
   path = '~/sync/Notes/',
   syntax = 'markdown',
@@ -15,6 +18,9 @@ local wiki = {
   -- set directories to be used
   path = '~/sync/wiki',
   path_html = '~/download/wiki_html',
+
+  -- make wiki base page named main, not index
+  index = 'main',
 
   -- update tag metadata on file save so you don't have to do it manually
   -- :VimwikiSearchTags to search, <C-X><C-O> in normal mode to omni complete :tags:
@@ -29,7 +35,7 @@ local wiki = {
 --   ...
 --   wiki_N,
 -- }
-vim.g.vimwiki_list = {
+vim_global.vimwiki_list = {
   wiki,
   notes,
 }
