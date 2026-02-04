c8d91a02240a3e9f0c0feee13a814e1f266c76ed
almond Heil <almond@almendra.dev>
2026-02-03 20:09:14 -0700

    fish: remove parent dir abbreviations

--- dot_config/fish/conf.d/abbreviations.fish
+++ dot_config/fish/conf.d/abbreviations.fish
@@ -1,7 +1,1 @@
-# abbreviations are like aliases only not.
-# they expand when you press enter in the command line.
-
-# replace more dots with more levels of parent dir, anywhere in the command
-abbr --add --position anywhere -- ... ../..
-abbr --add --position anywhere -- .... ../../..
-abbr --add --position anywhere -- ..... ../../../..
+abbr --add -- vim nvim
