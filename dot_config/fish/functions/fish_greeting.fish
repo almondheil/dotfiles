71585fc371e836dc758faebbb045235853ae2321
almond Heil <almond@almendra.dev>
2026-04-18 07:46:34 -0600

    fish: Add greeting message ASCII

---
+++ dot_config/fish/functions/fish_greeting.fish
@@ -0,0 +1,9 @@
+function fish_greeting
+  echo "\
+   --------------
+   |            |
+   | /\-/\/\-/\ | $(set_color -i)*despite everything, it's still you$(set_color --reset)
+   |/( - (     \|
+   ------/      -
+"
+end
