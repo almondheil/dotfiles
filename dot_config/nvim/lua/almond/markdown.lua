1dd78515698d0a06980ac289f54bd5fba3996d92
almond Heil <almond@almendra.dev>
2026-04-21 18:50:11 -0600

    nvim: Wrap markdown prettily

--- dot_config/nvim/lua/almond/markdown.lua
+++ dot_config/nvim/lua/almond/markdown.lua
@@ -2,3 +2,9 @@ require('nvim-mdlink').setup({
   keymap = true,
   cmp = true
 })
+
+-- set options when opening *.md
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+  pattern = '*.md',
+  command = 'setlocal wrap linebreak',
+})
